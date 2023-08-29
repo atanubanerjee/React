a7cbfeeb310dcecacbf1e7201121499961d2351c
Ubuntu <atanu@Reacting.j3ppsxzytd1exfb1fp5oqksx2g.phxx.internal.cloudapp.net>
2023-08-29 06:52:08 +0000

    New React project for Activity 2.1

---
+++ ReactKeyConcepts/2.1-present_yourself/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Hi, this is me - Atanu!
+      </header>
+      Right now, I live in Redmond, Washington.
+    </div>
+  );
+}
+
+export default App;
